b71510e4824a614547e301bf6aff644fe276fa7e
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-15 16:13:07 +0200

    job01 fini

---
+++ jour01/job01/index.php
@@ -0,0 +1,22 @@
+<?php
+$str = "LaPlateforme";
+$str2 = "Vive";
+$str3 = "!";
+echo $str2 . " " . $str . " " . $str3;
+?>
+
+<br>
+
+<?php
+$val = 6;
+$val += 4;
+echo $val;
+?>
+
+<br>
+
+<?php
+$myBool = false;
+echo $myBool;
+?>
+
