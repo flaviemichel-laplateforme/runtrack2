65a8fc9ad3e475674e6ae7fc2474e7a24a712f4c
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-16 14:03:41 +0200

    job02 fini

---
+++ jour02/job02/index.php
@@ -0,0 +1,14 @@
+<?php
+/* Job 02
+Afficher tous les nombres de 0 à 1337 compris SAUF 26, 37, 88, 1111 et 3233 en
+mettant un retour à la ligne entre chaque nombre (<br />)*/
+
+for ($i=0; $i <=1337; $i++) {
+    if ($i==26 || $i==37 || $i==88 || $i==1111 || $i==3233) {
+        continue;
+    }
+    echo $i . "<br />";
+}
+
+?>
+
