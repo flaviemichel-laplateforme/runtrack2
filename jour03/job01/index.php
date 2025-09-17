d2f73890e2ef46c4aac694cda7919b68515c9bf5
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-17 14:27:55 +0200

    job01 fini

--- jour03/job01/index.php
+++ jour03/job01/index.php
@@ -1,5 +1,14 @@
 <?php
 
 
-//TABLEAU 200, 204, 173, 98, 171, 404, 459.
+//TABLEAU 
+
 $tab = [200, 204, 173, 98, 171, 404, 459];
+foreach ($tab as $number) {
+    if ($number % 2 === 0) {
+        echo "$number est paire<br>";
+    } else {
+        echo "$number est impaire<br>";
+    }
+}
+?>
