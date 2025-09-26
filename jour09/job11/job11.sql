2a9a6716104e6265d1b63ac02309a0237f8a1580
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-26 13:59:31 +0200

    job11

--- jour09/job11/job11.sql
+++ jour09/job11/job11.sql
@@ -1,7 +1,5 @@
-Ecrivez dans le fichier “job11.sql” une requête permettant de sélectionner la somme
-des capacités des salles.
+-- Ecrivez dans le fichier “job11.sql” une requête permettant de sélectionner la somme
+-- des capacités des salles.
 
-SELECT etage, SUM(capacite) AS capacite_totale
-FROM salles 
-GROUP BY etage
-ORDER BY etage;
+SELECT SUM(capacite) 
+FROM salles;
